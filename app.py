e9d89b5bb39963e450202ab56aa7fcb445f18bca
ivan <ivanmantello@gmail.com>
2024-12-08 20:09:50 -0300

    cambio

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import joblib
 
 app = Flask(__name__)
@@ -6,18 +6,40 @@ model = joblib.load('modelo_precio.pkl')
 
 @app.route('/')
 def home():
-    return "Bienvenido a la API de predicción de precios. Usa la ruta '/predict' con una solicitud POST."
+    return render_template('index.html')
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    data = request.get_json()
-    tamaño = data['Tamaño_m2']
-    habitaciones = data['Habitaciones']
-    distancia = data['Distancia_centro_km']
+    tamaño = request.form['Tamaño_m2']
+    habitaciones = request.form['Habitaciones']
+    distancia = request.form['Distancia_centro_km']
     
+    # Convertir los datos a float para la predicción
+    tamaño = float(tamaño)
+    habitaciones = int(habitaciones)
+    distancia = float(distancia)
+
+    # Hacer la predicción
     prediction = model.predict([[tamaño, habitaciones, distancia]])
-    return jsonify({'Precio_predicho': prediction[0]})
+    
+    # Mostrar el resultado en la misma página
+    return render_template('index.html', prediccion=prediction[0])
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
